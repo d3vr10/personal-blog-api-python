0d04a9b8ec2147a1106734a8595c0dfe7bb5a437
d3vr10 <ablrcomx@gmail.com>
2025-01-03 21:18:51 -0500

    Wrap db init into fastapi lifespan function

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from lib.db.init import init
 from routers.posts.router import router as posts_router
 from routers.users.router import router as users_router
 
-init()
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    ## Init tasks
+    init()
+    yield
+    ## Cleanup
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(posts_router)
 app.include_router(users_router)
