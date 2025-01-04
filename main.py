7de174c1e828609ead00e6ecd92f12f1935a9bf0
d3vr10 <ablrcomx@gmail.com>
2025-01-03 21:20:07 -0500

    Add tags router

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from lib.db.init import init
 from routers.posts.router import router as posts_router
 from routers.users.router import router as users_router
+from routers.tags.router import router as tags_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,3 +15,4 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(posts_router)
 app.include_router(users_router)
+app.include_router(tags_router)
